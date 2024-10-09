31d6ce5355a9054fafaf3045663d578ab3c3ec2a
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-10-10 01:18:24 +0530

    Add

---
+++ constants/Colors.js
@@ -0,0 +1,5 @@
+export default {
+    primary: "#C2185B",
+    grey: "#888888",
+    dark: "#333333",
+};
---
+++ constants/affirmation-images.ts
@@ -0,0 +1,32 @@
+import californiaBackyardOne from "@/assets/affirmation-images/California-backyard-1.webp";
+import californiaBackyardTwo from "@/assets/affirmation-images/California-backyard-2.webp";
+import californiaBackyardThree from "@/assets/affirmation-images/California-backyard-3.webp";
+import californiaBackyardFour from "@/assets/affirmation-images/California-backyard-4.webp";
+
+import englishCountrysideOne from "@/assets/affirmation-images/english-countryside-1.webp";
+import englishCountrysideTwo from "@/assets/affirmation-images/english-countryside-2.webp";
+import englishCountrysideThree from "@/assets/affirmation-images/english-countryside-3.webp";
+import englishCountrysideFour from "@/assets/affirmation-images/english-countryside-4.webp";
+
+import mountainMeditateOne from "@/assets/affirmation-images/mountain-meditate-1.webp";
+import mountainMeditateTwo from "@/assets/affirmation-images/mountain-meditate-2.webp";
+import mountainMeditateThree from "@/assets/affirmation-images/mountain-meditate-3.webp";
+import mountainMeditateFour from "@/assets/affirmation-images/mountain-meditate-4.webp";
+
+
+
+export default {
+    californiaBackyardOne,
+    californiaBackyardTwo,
+    californiaBackyardThree,
+    californiaBackyardFour,
+    englishCountrysideOne,
+    englishCountrysideTwo,
+    englishCountrysideThree,
+    englishCountrysideFour,
+    mountainMeditateOne,
+    mountainMeditateTwo,
+    mountainMeditateThree,
+    mountainMeditateFour,
+   
+};
