f26789ce0d18a2b390540aac8e6244e856582d97
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-10-10 20:22:13 +0530

    Update affirmation-gallary.ts

--- constants/affirmation-gallary.ts
+++ constants/affirmation-gallary.ts
@@ -7,22 +7,22 @@ const AFFIRMATION_GALLERY = [
             {
                 id: 1,
                 text: "Every new day implies new opportunities to grow and excel. I am improving and working upon thyself. I aim to create a positive mindset. I am grateful for the journey and the lessons on the way.",
-                image: images.californiaBackyardOne,
+                image: images.img1,
             },
             {
                 id: 2,
                 text: "I am the architect of my destiny, shaping it with unwavering optimism and unrelenting drive. Every obstacle becomes a stepping stone on my path to excellence. I greet each moment with boundless energy and unshakeable faith in my abilities. My future beckons, full of endless possibilities.",
-                image: images.californiaBackyardTwo,
+                image: images.img2,
             },
             {
                 id: 3,
                 text: "I embody warmth and motivation, influencing those around me with my vibrant presence. My zest for life spreads effortlessly, propelling me towards triumph. I channel obstacles into opportunities, cultivating strength in every challenge. I dedicate myself to crafting a life filled with meaning and delight.",
-                image: images.californiaBackyardThree,
+                image: images.img3,
             },
             {
                 id: 4,
                 text: "I love and accept myself unconditionally, embracing my flaws and celebrating my strengths. I radiate positivity and attract abundance in all areas of my life. My heart is open, and my spirit is free. I am a beacon of light, shining brightly in the world.",
-                image: images.californiaBackyardFour,
+                image: images.img4,
             },
         ],
     },
@@ -32,22 +32,22 @@ const AFFIRMATION_GALLERY = [
             {
                 id: 5,
                 text: "I master my thoughts, deliberately embracing serenity instead of anxiety. With each breath, I quiet my mind and nurture my spirit. I let go of the need to fret about what lies beyond my reach. I stand firm in my ability to handle whatever life brings, remaining calm and centered in every moment.",
-                image: images.englishCountrysideOne,
+                image: images.img11,
             },
             {
                 id: 6,
                 text: "I cultivate tranquility as my inherent state of mind. My consciousness is unclouded, my spirit is buoyant, and I immerse myself fully in the present. Fearlessness resides within me, for I am steadfast and centered. I have faith in my capacity to navigate life's journey with grace.",
-                image: images.englishCountrysideTwo,
+                image: images.img12,
             },
             {
                 id: 7,
                 text: "With each passing day, I strengthen my resolve against stress and anxiety. I cultivate optimism and release doubts that hinder my growth. Love surrounds me, nurturing my inner peace. My resilience guides me through life's trials with poise and tranquility.",
-                image: images.englishCountrysideThree,
+                image: images.img13,
             },
             {
                 id: 8,
                 text: "As I exhale, I let go of all burdens, allowing my mind and body to surrender to serenity. I am enveloped in warmth and compassion, my spirit untethered from worry. With each deliberate step, I embody poise and inner calm. I choose to dwell in the here and now, where harmony and delight reside.",
-                image: images.englishCountrysideFour,
+                image: images.img14,
             },
         ],
     },
@@ -57,22 +57,22 @@ const AFFIRMATION_GALLERY = [
             {
                 id: 9,
                 text: "My potential knows no bounds, and every stride I take propels me towards excellence. My unwavering commitment and relentless efforts form the foundation of my triumphs. While others see hurdles, I envision pathways to progress. My journey towards success is my lifeblood, fueled by passion and sustained by unwavering resolve.",
-                image: images.mountainMeditateOne,
+                image: images.img21,
             },
             {
                 id: 10,
                 text: "I am the architect of my future, magnetizing achievement with ease. My aspirations are vivid, my objectives are defined, and every action I take resonates with my highest calling. I honor each milestone, no matter how modest, as a foundation for my grand accomplishment. Unyielding in spirit, I recognize that my capacity knows no bounds.",
-                image: images.mountainMeditateTwo,
+                image: images.img22,
             },
             {
                 id: 11,
                 text: "I have faith in my capabilities and surrender to the unfolding of my success story. Difficulties become catalysts for evolution, refining my character and sharpening my skills. I remain resolute in pursuit of my objectives, my ambition fueling every step. Success finds its way to me effortlessly, for I have cultivated readiness and earned my place among achievers.",
-                image: images.mountainMeditateThree,
+                image: images.img23,
             },
             {
                 id: 12,
                 text: "I am a beacon of prosperity, drawing fulfillment into every aspect of my life. My outlook is optimistic, and my endeavors are thoughtful. From each encounter, I glean wisdom, constantly elevating my potential. Success is not merely a target; it's the tapestry woven from daily moments of triumph and growth.",
-                image: images.mountainMeditateFour,
+                image: images.img24,
             },
         ],
     }
