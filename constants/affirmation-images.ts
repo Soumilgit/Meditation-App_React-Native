8f539f509fc20bcad8e1bb9fdaf70af516890282
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-10-10 20:22:33 +0530

    Update affirmation-images.ts

--- constants/affirmation-images.ts
+++ constants/affirmation-images.ts
@@ -1,32 +1,32 @@
-import californiaBackyardOne from "@/assets/affirmation-images/California-backyard-1.webp";
-import californiaBackyardTwo from "@/assets/affirmation-images/California-backyard-2.webp";
-import californiaBackyardThree from "@/assets/affirmation-images/California-backyard-3.webp";
-import californiaBackyardFour from "@/assets/affirmation-images/California-backyard-4.webp";
+import img1 from "@/assets/affirmation-images/img1.webp";
+import img2 from "@/assets/affirmation-images/img2.webp";
+import img3 from "@/assets/affirmation-images/img3.webp";
+import img4 from "@/assets/affirmation-images/img4.webp";
 
-import englishCountrysideOne from "@/assets/affirmation-images/english-countryside-1.webp";
-import englishCountrysideTwo from "@/assets/affirmation-images/english-countryside-2.webp";
-import englishCountrysideThree from "@/assets/affirmation-images/english-countryside-3.webp";
-import englishCountrysideFour from "@/assets/affirmation-images/english-countryside-4.webp";
+import img11 from "@/assets/affirmation-images/img11.webp";
+import img12 from "@/assets/affirmation-images/img12.webp";
+import img13 from "@/assets/affirmation-images/img13.webp";
+import img14 from "@/assets/affirmation-images/img14.webp";
 
-import mountainMeditateOne from "@/assets/affirmation-images/mountain-meditate-1.webp";
-import mountainMeditateTwo from "@/assets/affirmation-images/mountain-meditate-2.webp";
-import mountainMeditateThree from "@/assets/affirmation-images/mountain-meditate-3.webp";
-import mountainMeditateFour from "@/assets/affirmation-images/mountain-meditate-4.webp";
+import img21 from "@/assets/affirmation-images/img21.webp";
+import img22 from "@/assets/affirmation-images/img22.webp";
+import img23 from "@/assets/affirmation-images/img23.webp";
+import img24 from "@/assets/affirmation-images/img24.webp";
 
 
 
 export default {
-    californiaBackyardOne,
-    californiaBackyardTwo,
-    californiaBackyardThree,
-    californiaBackyardFour,
-    englishCountrysideOne,
-    englishCountrysideTwo,
-    englishCountrysideThree,
-    englishCountrysideFour,
-    mountainMeditateOne,
-    mountainMeditateTwo,
-    mountainMeditateThree,
-    mountainMeditateFour,
+    img1,
+    img2,
+    img3,
+    img4,
+    img11,
+    img12,
+    img13,
+    img14,
+    img21,
+    img22,
+    img23,
+    img24,
    
 };
