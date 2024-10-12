60ae3e3f2b5e8184953ef4d09faeed7ede4f8d86
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-10-12 18:49:59 +0530

    Update _layout.tsx

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -1,45 +1,52 @@
-import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
-import Colors from "@/constants/Colors";
-import React from "react";
+import TimerProvider from "@/context/TimerContext";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export const unstable_settings = {
-    // Ensure that reloading on `/modal` keeps a back button present.
-    initialRouteName: "(tabs)",
-};
+// this will prevent the flash screen from auto hiding until loading all the assets is complete
+SplashScreen.preventAutoHideAsync();
 
-const Page = () => {
+export default function RootLayout() {
+    const [fontsLoaded, error] = useFonts({
+        "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+    });
+
+    useEffect(() => {
+        if (error) throw error;
+        if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    if (!fontsLoaded && !error) {
+        return null;
+    }
+
     return (
-        <Tabs
-            screenOptions={{
-                headerShown: false,
-                tabBarActiveTintColor: Colors.primary,
-            }}
-        >
-            <Tabs.Screen
-                name="nature-meditate"
-                options={{
-                    tabBarLabel: "Meditate",
-                    tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons
-                            name="flower-tulip"
-                            size={24}
-                            color={color}
-                        />
-                    ),
-                }}
-            />
-            <Tabs.Screen
-                name="affirmations"
-                options={{
-                    tabBarLabel: "Affirmations",
-                    tabBarIcon: ({ color }) => (
-                        <Entypo name="open-book" size={24} color={color} />
-                    ),
-                }}
-            />
-        </Tabs>
+        <SafeAreaProvider>
+            <TimerProvider>
+                <Stack>
+                    <Stack.Screen
+                        name="(tabs)"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="meditate/[id]"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="index"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="(modal)/adjust-meditation-duration"
+                        options={{ headerShown: false, presentation: "modal" }}
+                    />
+                </Stack>
+            </TimerProvider>
+        </SafeAreaProvider>
     );
-};
-
-export default Page;
+}
